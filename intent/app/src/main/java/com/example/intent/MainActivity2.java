042b63f466fd61d23d1ed7aab8db16d4da544dcf
Agha-Basit-Khan <bsef18m036@pucit.edu.pk>
2021-06-08 02:18:54 +0500

    intent update

---
+++ intent/app/src/main/java/com/example/intent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void ShowWebsite(View view){
+        Uri uri =
+    }
+}
